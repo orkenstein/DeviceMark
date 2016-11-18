c74fd7c1214a0be792dae782fa23e4a0189aada6
Ruslan K <rsl@futureuniverse.com>
2016-11-18 14:50:23 +0100

    Some initial setup: bundler + fastlane + cocoapods

---
+++ DeviceMark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeviceMark
+//
+//  Created by Ruslan K on 18/11/2016.
+//  Copyright © 2016 Ruslan K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
